0c6e4f3f1953862a2e512f6acd41b14687ea5ae3
Dan Mosora <dan@stitchdata.com>
2018-06-27 00:32:49 -0400

    Starting skeleton of spec

--- burler/__init__.py
+++ burler/__init__.py
@@ -1,29 +1,9 @@
-# Streams decorators
-def singer_stream(cls):
-    # Adds base call to different pieces that make up a stream.
-    # Can a class be constructed declaratively in python?
-    pass
-
 # Class that describes the basic structure of a "tap"
 class Tap:
-    def __init__(self): # Keyword args? Some kind of set up config?
-        self.config = None # Either a function that validates and sets config, a Schema object, or if just an array of values, do no validation and provide it via a __get__()
-        self.stream_builder = lambda _: pass # Function that configures a list of streams, either through Extend, or add() that takes a stream
-        # Other things required?
-    
-    def with_streams(self, streams):
-        pass
-
-    def run(self):
-        pass
-    
-    def _do_discover(self):
-        # Detect what has been configured and call it according to a pattern
-        streams_with_discovery = [s for s in self.streams]
-        pass
-
-    def _do_sync(self):
-        # Detect what has been configured and call it according to a pattern
+    def __init__(config_spec=None):
+        # Interpret args
+        # Validate schema if: Schema, matches basic data types in config example, or call function
+        # configured with config as parameter, if None, then validation is a no-op
         pass
 
 def run_tap(args):
@@ -33,3 +13,8 @@ def run_tap(args):
     # Can we discover a class implementing a subclass from a function call? Meta has to support that
     pass
 
+def tap():
+    # Create the tap "app" and return it
+    # This might also be able to set the main entrypoint of the tap?
+    # That way the user doesn't need to wrap for exceptions, or anything, but they can specify it!
+    pass
