b8d0aff280b389954cee10e7dc42f31b7df1cd34
Dan Mosora <dan@stitchdata.com>
2018-08-15 01:15:29 -0400

    End to end test of declaring streams and running sync mode based on them

--- burler/streams.py
+++ burler/streams.py
@@ -5,10 +5,19 @@ from burler.exceptions import DuplicateStream
 def _raise_duplicate_stream(name):
     raise DuplicateStream("Attempted to register duplicate stream ({}) using Stream class. For a single class that handles multiple stream names, use the decorator with a unique name:\n\n\t@stream(\"my_stream\", tap_stream_id=\"invoices\")\n\t@stream(\"my_stream\", tap_stream_id=\"contacts\")\n\tclass MyStream():\n\t\t...".format(name))
 
-def stream(name, tap_stream_id=None, stream_alias=None):
+__pascal_case_converter = re.compile(r'(.)([A-Z][a-z]+?)')
+
+def __class_name_to_underscore(name):
+    return __pascal_case_converter.sub(r'\1_\2', name).lower()
+
+def stream(name=None, tap_stream_id=None, stream_alias=None):
     """ A class decorator that registers a Stream class with the tap. """
 
     def wrapped_stream(cls):
+        nonlocal name
+        if not name:
+            name = __class_name_to_underscore(cls.__name__)
+
         class StreamMetadata(object):
             """ Metadata class that allows the Tap to use values configured during the tap run, and allows decorators to modify the definition of a stream's methods on the fly. """
             def __init__(self, cls, name, tap_stream_id, stream_alias):
@@ -17,9 +26,10 @@ def stream(name, tap_stream_id=None, stream_alias=None):
                 self.display_name = lambda: name
                 self.emitted_name = lambda: stream_alias or tap_stream_id
 
-            def set_context():
+            def set_context(self, instance):
                 """ Sets the context to be used when calling the sync methods. This gives it bookmarks, catalog, etc. """
-                pass
+                current_tap = Tap._Tap__tap
+                instance.client = current_tap.client
 
         smd = StreamMetadata(cls, name, tap_stream_id, stream_alias)
         if smd.unique_name() in Tap.streams:
@@ -28,22 +38,17 @@ def stream(name, tap_stream_id=None, stream_alias=None):
         # Register cls as a stream (add to tap's map of stream classes)
         Tap.streams[smd.unique_name()] = smd
 
-        # Set its set_context function (for the configured names)
         # Make it a subclass of Stream?
         # Provide it with implementations of sync and get_bookmark, etc that can be overriden
+
         return cls
     return wrapped_stream
 
 class StreamMeta(type):
-    __pascal_case_converter = re.compile(r'(.)([A-Z][a-z]+?)')
-
-    def __class_name_to_underscore(cls, name):
-        return cls.__pascal_case_converter.sub(r'\1_\2', name).lower()
-
     def __init__(cls, name, bases, clsdict):
         # Check this out: https://stackoverflow.com/questions/18126552/how-to-run-code-when-a-class-is-subclassed
         if len(bases) > 0: # We only want to register classes that are sub-sub classed from this
-            underscore_cls_name = cls.__class_name_to_underscore(name)
+            underscore_cls_name = __class_name_to_underscore(name)
             if underscore_cls_name in Tap.streams:
                 _raise_duplicate_stream(underscore_cls_name)
             stream(underscore_cls_name)(cls) # Call decorator wrapper explicitly
--- burler/taps.py
+++ burler/taps.py
@@ -1,7 +1,13 @@
+import json
+import sys
 
 from burler.exceptions import ConfigValidationException, SyncModeNotDefined, MissingCatalog, NoClientConfigured
 
+import singer
 import singer.logger as logging
+from singer import metrics
+from singer import metadata
+from singer import Transformer
 
 from schema import Schema as SSchema, SchemaError
 from voluptuous import Schema as VSchema, Invalid
@@ -9,6 +15,7 @@ from voluptuous import Schema as VSchema, Invalid
 LOGGER = logging.get_logger()
 
 # Class that describes the basic structure of a "tap"
+# TODO: Load streams from module? Looks in the working directory, finds the specified module and loads the streams? To eliminate the "from tap.streams import *" junk...
 class Tap:
     __tap = None
 
@@ -16,8 +23,13 @@ class Tap:
     debug_mode = False
     config = None
     requires_config = True
+    json_encoder = None
+
+    _discovery_override = False
+    _sync_override = False
     _validate = lambda c: self.log_debug(LOGGER.warn, "No configuration spec provided, skipping further validation...") or c # Returns c
-    _discover = lambda _: self.log_debug(LOGGER.warn, "No discovery mode specified, if needed define one with the decorator @tap.discovery_mode, skipping discovery")
+    # TODO: Check if discovery is required with a class function
+    _discover = lambda _: self.log_debug(LOGGER.warn, "No discovery mode specified. If needed, define one with the decorator @tap.discovery_mode, skipping discovery")
     _sync = lambda _1, _2, _3: self._raise(SyncModeNotDefined, "No sync mode specified, please define one with the decorator @tap.sync_mode")
 
     def _log_if_debug(self, log_func, message):
@@ -39,8 +51,9 @@ class Tap:
         _validate_basic_conf_practices(conf)
         return _validate(conf)
 
-    def __init__(self, config_spec=None, requires_catalog=True, debug=False):
+    def __init__(self, config_spec=None, requires_catalog=True, debug=False, json_encoder=None):
         self.requires_catalog = requires_catalog
+        self.json_encoder = json_encoder
 
         if config_spec is None:
             self.requires_config = False
@@ -91,29 +104,117 @@ class Tap:
 
         self.debug_mode = debug
 
+    def __discover_using_registered_streams(self, config):
+        LOGGER.info("Starting discover")
+
+        streams = []
+        for smd in self.streams.values(): # s is StreamMetadata
+            instance = smd.cls()
+            smd.set_context(instance)
+            # TODO: Check to see that load_schema and load_metadata are specified on these classes
+            streams.append({'stream': smd.display_name(), 'tap_stream_id': smd.unique_name(), 'schema': instance.load_schema(), 'metadata': instance.load_metadata()})
+
+        catalog = {"streams": streams}
+        json.dump(catalog, sys.stdout, indent=2)
+        LOGGER.info("Finished discover")
+
     def do_discover(self, config):
         """ Main entrypoint for discovery mode. """
         self.validate_config(config)
         self.config = config
-        # TODO: Use configured streams and embark on standard discovery process
-        self._discover(config)
+
+        if self._discovery_override:
+            LOGGER.info("Found decorated discovery method, running discovery mode...")
+            self._discover(config)
+            return
+
+        # TODO: How to check if discovery mode is not available? If no load_schema or load_metadata appear on any of the classes?
+        self.__discover_using_registered_streams(config)
+
+    def __sync_using_registered_streams(self, config, state, catalog):
+        def process_record(record):
+            """ Serializes data into Python objects via custom encoder. """
+            if self.json_encoder:
+                rec_str = json.dumps(record, cls=self.json_encoder)
+                rec_dict = json.loads(rec_str)
+                return rec_dict
+            return record
+
+        def stream_is_selected(mdata):
+            return mdata.get((), {}).get('selected', False)
+
+        for stream in catalog.streams:
+            stream_name = stream.tap_stream_id
+            mdata = metadata.to_map(stream.metadata)
+            if not stream_is_selected(mdata):
+                LOGGER.info("%s: Skipping - not selected", stream_name)
+                continue
+
+            singer.write_state(state)
+            key_properties = metadata.get(mdata, (), 'table-key-properties')
+            singer.write_schema(stream_name, stream.schema.to_dict(), key_properties)
+
+            LOGGER.info("%s: Starting sync", stream_name)
+            # Refactor Note: Start of stream sync
+            smd = self.streams[stream.tap_stream_id]
+            instance = smd.cls()
+            smd.set_context(instance)
+
+            # If we have a bookmark, use it; otherwise use start_date
+            ## META: Write stream into state object's bookmarks map
+            if (instance.replication_method == 'INCREMENTAL' and
+                    not state.get('bookmarks', {}).get(stream.tap_stream_id)):
+                singer.write_bookmark(state,
+                                      stream.tap_stream_id,
+                                      instance.replication_key,
+                                      config.get('start_date'))
+
+            ## META: Check if "sync" is defined
+            with metrics.record_counter(stream.tap_stream_id) as counter:
+                # TODO: This will need to unwrap (StreamClass, record) for sub-streams
+                for record in instance.sync(state):
+                    counter.increment()
+
+                    rec = process_record(record)
+                    with Transformer() as transformer:
+                        rec = transformer.transform(rec, stream.schema.to_dict(), metadata.to_map(stream.metadata))
+
+                    singer.write_record(stream.tap_stream_id, rec)
+
+                ## META: Bookmark stratey comes in here
+                if instance.replication_method == "INCREMENTAL":
+                    singer.write_state(state)
+
+                LOGGER.info("%s: Completed sync (%s rows)", stream_name, counter.value)
+            # Refactor Note: End of stream sync
+
+        singer.write_state(state)
+        LOGGER.info("Finished sync")
 
     def do_sync(self, config, state, catalog):
         """ Main entrypoint for sync mode. """
         self.validate_config(config)
         self.config = config
+
         if self.requires_catalog and catalog is None:
-            raise MissingCatalog("This tap requires a catalog to be specified using --catalog <file.json>. Please provide a catalog file.")
-        # TODO: Use configured streams and embark on standard sync process
-        self._sync(config, state, catalog)
+            raise MissingCatalog("Please provide a catalog file. This tap requires a catalog to be specified using --catalog <file.json>.")
+
+        if self._sync_override:
+            LOGGER.info("Found decorated sync method, running sync mode...")
+            self._sync(config, state, catalog)
+            return
+
+        self.__sync_using_registered_streams(config, state, catalog)
 
     # Tap decorators
     def discovery_mode(self, func):
         self._discover = func
+        self._discovery_override = True
         return func
 
     def sync_mode(self, func):
         self._sync = func
+        self._sync_override = True
         return func
 
     # NB: It seems that properties are configured at class creation, so to have
